616a7ed08260d3332127543f0223f2f726675c5e
Francisco <fco.pena.violin@gmail.com>
2020-07-13 22:21:47 -0600

    Controlador cerrar sesión

---
+++ Project_List/src/java/database/controladorsalir.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Francisco
+ */
+public class controladorsalir extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession cerrar_sesion = request.getSession();
+        cerrar_sesion.removeAttribute("id_user");
+        cerrar_sesion.removeAttribute("s_user");
+        cerrar_sesion.invalidate();
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Description";
+    }
+}
